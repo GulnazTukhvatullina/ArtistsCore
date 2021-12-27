2ce14ddbad7bcb4e0bff15cf7eb28f73cd0a8ec9
gulnaz.tukhvatullina.02@inbox.ru <gulnaz.tukhvatullina.02@inbox.ru>
2021-12-27 12:29:24 +0300

    Создание ядра приложения

---
+++ ArtistsCore/DataAccess.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections.ObjectModel;
+
+namespace ArtistsCore
+{
+    class DataAccess
+    {
+        public static ArtistEntities connection = new ArtistEntities();
+
+        public List<Artist> GetArtist()
+        {
+            return new List<Artist>(connection.Artist);
+        }
+
+        public List<Exhibition> GetExhibitions()
+        {
+            return new List<Exhibition>(connection.Exhibition);
+        }
+
+        public List<Hall> GetHalls()
+        {
+            return new List<Hall>(connection.Hall);
+        }
+
+        public List<Institution> GetInstitutions()
+        {
+            return new List<Institution>(connection.Institution);
+        }
+
+        public List<Institution_Hall> GetInstitution_Halls()
+        {
+            return new List<Institution_Hall>(connection.Institution_Hall);
+        }
+
+        public List<Painting_artist> GetPainting_Artists()
+        {
+            return new List<Painting_artist>(connection.Painting_artist);
+        }
+
+        public List<Type_exhibition> GetType_Exhibitions()
+        {
+            return new List<Type_exhibition>(connection.Type_exhibition);
+        }
+    }
+}
