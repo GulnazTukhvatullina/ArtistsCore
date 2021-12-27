9b45922246fb217b0302e807ecf5066e30a4d39e
gulnaz.tukhvatullina.02@inbox.ru <gulnaz.tukhvatullina.02@inbox.ru>
2021-12-27 11:48:00 +0300

    Добавьте файлы проекта.

---
+++ ArtistsCore/bd_connections.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections.ObjectModel;
+
+namespace ArtistsCore
+{
+    public class bd_connections
+    {
+        public static ArtistEntities connection = new ArtistEntities();
+    }
+}
